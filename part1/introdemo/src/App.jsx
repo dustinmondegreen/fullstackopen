1d1adafb26852d341bc2429c0ecffb1fe3d712a4
dustinmondegreen <antowilliam1029@gmail.com>
2025-07-09 18:09:32 +0700

    Add vite web dev environment with React
    - Condense Main.jsx and App.jsx
    - Remove Main.css and App.css

---
+++ part1/introdemo/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
+
+// This is a simple component that returns html code that 
+// displays hello world encased in a div
+
+
---
+++ part1/introdemo/src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+// This will import the App component and render it into root div of index.html
