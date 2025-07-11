b365fc5015053322c0342df6302c7d43af648aa7
dustinmondegreen <antowilliam1029@gmail.com>
2025-07-11 11:20:26 +0700

    Add Unicafe directory for exercise 1.6 - 1.14
    - Finish exercise 1.6 Unicafe state traffics
    - Finish exercise 1.7 add average & positive

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const Average =({good, neutral, bad}) => {
+  const resGood = good * 1
+  const resNeutral = 0
+  const resBad = bad * (-1)
+  const total = good + neutral + bad
+  return(
+    <p>Average {(resGood + resNeutral + resBad) / total}</p>
+  )
+}
+
+const Positive =({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  return(
+    <p>Good {((good) / total) * 100}%</p>
+
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return(
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => (setGood(good + 1))}>good</button>
+      <button onClick={() => (setNeutral(neutral + 1))}>neutral</button>
+      <button onClick={() => (setBad(bad + 1))}>bad</button>
+
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>total {good + neutral + bad}</p>
+      <Average good={good} neutral={neutral} bad={bad}/>
+      <Positive good={good} neutral={neutral} bad={bad}/>
+
+
+    </div>
+  )
+}
+
+export default App
