e7cd91190028426bafd9dae2891fda97658b62d7
dustinmondegreen <antowilliam1029@gmail.com>
2025-07-17 21:29:40 +0700

    Add axios and Effect hook as promise for phonebook

--- part2/test/src/App.jsx
+++ part2/test/src/App.jsx
@@ -1,64 +1,35 @@
-import Course from './components/Course'
+import { useState, useEffect } from "react";
+import axios from 'axios'
+import Note from './components/Note'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
-  const courseList = courses.map(
-    (course) => {
-      return(
-        <Course key={course.id} course={course}/>
-      )
-    }
-  )
+  useEffect(() => {
+    console.log('effect');
+    axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+      console.log(response)
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+  }, [])
 
-  return(
-    <>
-    {courseList}
-    </>
+  console.log('render', notes.length, 'notes')
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+    </div>
   )
 }
 
-export default App
+export default App
